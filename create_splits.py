35b2520ce113a2e692f7f1e8715586a4aaccbd67
varam48 <65580812+varam48@users.noreply.github.com>
2022-12-29 20:19:08 +0100

    Update create_splits.py
    create_splits.py 
    8:1:1 split

--- create_splits.py
+++ create_splits.py
@@ -18,6 +18,23 @@ def split(source, destination):
         - destination [str]: destination data directory, contains 3 sub folders: train / val / test
     """
     # TODO: Implement function
+    
+    source = data_dir + str("/training_and_validation/")
+    dest_train = data_dir + str("/train/")
+    dest_val = data_dir + str("/val/")
+    dest_test = data_dir + str("/test/")
+        
+    get_files = os.listdir(source)                     
+    j=0  
+    # 80% of the files will be placed in train, 10% in val and 10% in test
+    for i in get_files:
+        if j<10:
+            shutil.move(source + i, dest_val) 
+        elif j>=10 and j <20:
+            shutil.move(source + i, dest_test)
+        else:    
+            shutil.move(source + i, dest_train) 
+        j = j+1
 
 
 if __name__ == "__main__":
